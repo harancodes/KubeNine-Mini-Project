44e32621e3c505a9fb2fece540b9d417e6aa5273
Hariharan M <hrihrnofficial@gmail.com>
2026-03-03 04:32:26 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return jsonify({"status":"ok", "message" : "Hello from Eks"})
+
+if  __name__ == "__main__":
+    app.run(host="0.0.0.0", port = 5000)
---
+++ terraform/outputs.tf
@@ -0,0 +1,29 @@
+output "cluster_name" {
+  description = "EKS Cluster Name"
+  value       = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  description = "EKS API Server Endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "Security Group ID attached to the EKS cluster"
+  value       = module.eks.cluster_security_group_id
+}
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnets" {
+  description = "Public Subnet IDs"
+  value       = module.vpc.public_subnets
+}
+
+output "private_subnets" {
+  description = "Private Subnet IDs"
+  value       = module.vpc.private_subnets
+}
